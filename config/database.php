d1ac12c90cf4314f0ffef55fb30086a81f5d97b2
Miles Pieterse <milesyusuf123@gmail.com>
2023-07-30 13:49:01 +0200

    Add: configuration file that handles database connection

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../config.php';
+?>
+
+<?php
+$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check the connection for errors
+if ($conn->connect_error) {
+    die( "Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connection successful!";
+}
+
+// Set the character set to handle special characters properly
+$conn->set_charset("utf8");
+?>
