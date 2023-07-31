3ffee5dd7bec0efb3fdbf71d9a44abed6f74633d
Miles Pieterse <milesyusuf123@gmail.com>
2023-07-31 23:42:41 +0200

    Add: script for handling user logins which directs to necessary page based on credential type

---
+++ include/loginProcess.php
@@ -0,0 +1,46 @@
+<?php require_once "../config/database.php" ?>
+
+
+<?php
+$errors = [];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["password"])) {
+        $errors[] = "Please fill in all input fields";
+    } else {
+        // Input data sanitization
+        $name = mysqli_real_escape_string($conn, $_POST["name"]);
+        $email = mysqli_real_escape_string($conn, $_POST["email"]);
+        $password = mysqli_real_escape_string($conn, $_POST["password"]);
+        // ...
+
+        // Query to check if user is an "admin" or "member"
+        $sql = "SELECT * FROM users WHERE name = '$name' AND email = '$email' AND password = '$password'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            if ($user["is_admin"] == 1) {
+                header('location: ../views/librarian/landingPage.php');
+                exit();
+            } else {
+                header('location: ../views/member/landingPage.php');
+                exit();
+            }
+        } else {
+            $errors[] = "User info is incorrect...😐";
+        }
+        // ...
+    }
+}
+?>
+
+<?php
+// Check to see if errors occurred
+if (!empty($errors)) {
+    foreach ($errors as $error) {
+        echo $error . "</br>";
+    }
+}
+// ...
+?>
