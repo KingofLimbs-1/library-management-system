61d518609af61a3121c57f56da04dfc491c7f0e2
Miles Pieterse <milesyusuf123@gmail.com>
2023-08-03 01:16:11 +0200

    Add: Landing page for librarian/admins

---
+++ views/librarian/landingPage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="/projects/library-management-system/assets/css/librarian/landingPage.css">
+    <link rel="stylesheet" href="/assets/css/fonts.css">
+</head>
+
+<body>
+    <div class="logo">
+    </div>
+    <section class="dashboardSection">
+        <div>
+            <h1>Welcome, Librarian</h1>
+        </div>
+        <div class="dashboardContainer">
+            <div class="dashItem">
+                <a href="../librarian/viewMembers.php">
+                    <h1>Manage Members</h1>
+                </a>
+            </div>
+            <div class="dashItem">
+                <a href="../librarian/viewBooks.php">
+                    <h1>Manage Books</h1>
+                </a>
+            </div>
+            <div class="dashItem">
+                <a href="../librarian/viewAdmin.php">
+                    <h1>Manage Librarians</h1>
+                </a>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
