6f3e88bbf44cb7d0fac7d6638b0252bfddab32f8
Miles Pieterse <milesyusuf123@gmail.com>
2023-08-07 20:54:01 +0200

    Update: class with methods that handle admins being added to users table

--- classes/librarian.php
+++ classes/librarian.php
@@ -15,7 +15,7 @@ class crudOperations
     }
     // ...
 
-    // Methods
+    // BOOK METHODS
 
     // Add a new book
     public function addBook($title, $author, $isbn)
@@ -50,5 +50,63 @@ class crudOperations
         }
     }
     // ...
+
+    // Edit book
+    public function editBook($title, $author, $isbn)
+    {
+        $sql = "UPDATE books SET title = ?, author = ?, isbn = ? WHERE book_id = ?";
+        $result = $this->conn->prepare($sql);
+        $result->bind_param('sssi', $title, $author, $isbn);
+
+        $check = $result->execute();
+
+        if ($check) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // ...
+    // BOOK METHODS END
+
+
+    // ADMIN METHODS
+
+    // Check to verify if admin being added already exists in DB
+    public function checkUsers($name, $email)
+    {
+        $sql = "SELECT * FROM users WHERE name = ? AND email = ?";
+        $result = $this->conn->prepare($sql);
+        $result->bind_param('ss', $name, $email);
+
+        $result->execute();
+        $result->store_result();
+
+        if ($result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // ...
+
+    // Add a new admin
+    public function addAdmin($name, $email, $password)
+    {
+        $sql = "INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, 1)";
+        $result = $this->conn->prepare($sql);
+        $result->bind_param('sss', $name, $email, $password);
+
+        $check = $result->execute();
+
+        if ($check) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // ...
+
+    // ADMIN METHODS END
 }
 ?>
