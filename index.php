a21ba7f8badfc2d19ae585a15cb3262c04949c0c
Miles Pieterse <milesyusuf123@gmail.com>
2023-08-07 23:39:56 +0200

    Update: user landing page with necessary static view changes

--- index.php
+++ index.php
@@ -5,60 +5,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library Management System</title>
+    <link rel="stylesheet" href="./assets/css/fonts.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
 </head>
 
 <body>
     <div class="logo">
-        <?php include './include/logo.php'; ?>
+
     </div>
 
-    <div class="login">
-        <form action="include/loginProcess.php" method="post">
-            <h2>Login</h2>
-            <div>
-                <label>Name</label>
-                <input type="text" id="name" name="name" value="">
-            </div>
+    <section class="formsContainer">
+        <div class="login">
+            <form action="include/loginProcess.php" method="post">
+                <h2 class="heading">Login</h2>
 
-            <div>
-                <label>Email</label>
-                <input type="text" id="email" name="email" value="">
-            </div>
+                <div>
+                    <label>name</label>
+                    <input type="text" id="name" name="name" value="">
+                </div>
 
-            <div>
-                <label>Password</label>
-                <input type="password" id="password" name="password" value="">
-            </div>
+                <div>
+                    <label>email address</label>
+                    <input type="text" id="email" name="email" value="">
+                </div>
 
-            <div>
-                <input type="submit" value="Login">
-            </div>
-        </form>
-    </div>
+                <div>
+                    <label>password</label>
+                    <input type="password" id="password" name="password" value="">
+                </div>
 
-    <div class="signUp">
-        <form action="#">
-            <h2>Sign Up</h2>
-            <div>
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name" value="">
-            </div>
+                <div>
+                    <input type="submit" value="Sign in">
+                </div>
+            </form>
+        </div>
 
-            <div>
-                <label for="email">Email</label>
-                <input type="text" id="email" name="email" value="">
-            </div>
+        <hr>
+        <div class="signUp">
+            <form action="include/registerProcess.php" method="post">
+                <h2 class="heading">Register</h2>
+                <div>
+                    <label for="name">name</label>
+                    <input type="text" id="name" name="name" value="">
+                </div>
 
-            <div>
-                <label for="password">Password</label>
-                <input type="text" id="password" name="password" value="">
-            </div>
+                <div>
+                    <label for="email">email address</label>
+                    <input type="text" id="email" name="email" value="">
+                </div>
 
-            <div>
-                <input type="submit" value="Register">
-            </div>
-        </form>
-    </div>
+                <div>
+                    <label for="password">password</label>
+                    <input type="text" id="password" name="password" value="">
+                </div>
+
+                <div>
+                    <input type="submit" value="Sign up">
+                </div>
+            </form>
+        </div>
+    </section>
 </body>
 
 </html>
