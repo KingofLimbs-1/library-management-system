11fd9903dfb0d7e308961c0c298577e7c154a7c8
Miles Pieterse <milesyusuf123@gmail.com>
2023-08-07 02:20:11 +0200

    Update: function to delete books from database

--- classes/librarian.php
+++ classes/librarian.php
@@ -29,11 +29,26 @@ class crudOperations
         if ($check) {
             return true;
         } else {
-            echo "Error" . $result->error;
             return false;
         }
     }
+    // ...
 
+    // Delete a book
+    public function deleteBook($bookId)
+    {
+        $sql = "DELETE FROM books WHERE book_id = ?";
+        $result = $this->conn->prepare($sql);
+        $result->bind_param('i', $bookId);
+
+        $check = $result->execute();
+
+        if ($check) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // ...
 }
 ?>
